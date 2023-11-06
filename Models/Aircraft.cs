945dbe6102c03083a3c9aadf9f14002c749a975b
Shohboz Xurramov <shohboztuit@gmail.com>
2023-11-06 23:19:25 +0500

    Add project files.

---
+++ Models/Aircraft.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Models
+{
+    abstract class Aircraft
+    {
+        public string Color { get; set; }
+        public Object engine { get; set; }
+        public abstract void Start();
+        public abstract void Stop();
+        public abstract void TakeOff();
+        public abstract void Land();
+
+
+    }
+}
---
+++ Models/FighterPlane.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Models
+{
+    internal class FighterPlane : Aircraft
+    {
+        public object engine { get; set; }
+        public override void Land()
+        {
+            Console.WriteLine("PassengerPlane Landing............");
+        }
+
+        public override void Start()
+        {
+            Console.WriteLine("PassengerPlane Starting...........");
+        }
+
+        public override void Stop()
+        {
+            Console.WriteLine("PassengerPlane Stoping.........");
+        }
+
+        public override void TakeOff()
+        {
+            Console.WriteLine("PassengerPlane trkeoff............");
+        }
+    }
+}
